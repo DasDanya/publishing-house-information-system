39a0c6d4e4ada613e0c807928e500fb1ce92119a
DasDanya <klimachkov.danilka@mail.ru>
2022-11-01 23:37:58 +0300

    Добавил элементы в форму типографии. Сделал методы на проверку введённых данных

--- PublishingHouse/PublishingHouse/PrintingHouseMenu.Designer.cs
+++ PublishingHouse/PublishingHouse/PrintingHouseMenu.Designer.cs
@@ -33,11 +33,30 @@ namespace PublishingHouse
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.backTab = new System.Windows.Forms.ToolStripMenuItem();
             this.printingHouseDataGridView = new System.Windows.Forms.DataGridView();
-            this.Select = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.houseTextBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.streetTextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.phoneNumberTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputNameLabel = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.printingHouseDataGridView)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -50,7 +69,7 @@ namespace PublishingHouse
             this.backTab});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1274, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1425, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,12 +88,12 @@ namespace PublishingHouse
             this.printingHouseDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.printingHouseDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Select});
-            this.printingHouseDataGridView.Location = new System.Drawing.Point(12, 31);
+            this.printingHouseDataGridView.Location = new System.Drawing.Point(13, 32);
             this.printingHouseDataGridView.Name = "printingHouseDataGridView";
             this.printingHouseDataGridView.RowHeadersVisible = false;
             this.printingHouseDataGridView.RowHeadersWidth = 51;
             this.printingHouseDataGridView.RowTemplate.Height = 29;
-            this.printingHouseDataGridView.Size = new System.Drawing.Size(581, 348);
+            this.printingHouseDataGridView.Size = new System.Drawing.Size(741, 305);
             this.printingHouseDataGridView.TabIndex = 1;
             // 
             // Select
@@ -86,47 +105,232 @@ namespace PublishingHouse
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.houseTextBox);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.streetTextBox);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.cityTextBox);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.stateTextBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.emailTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.phoneNumberTextBox);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.nameTextBox);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(632, 31);
+            this.groupBox1.Controls.Add(this.inputNameLabel);
+            this.groupBox1.Location = new System.Drawing.Point(773, 31);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(630, 338);
+            this.groupBox1.Size = new System.Drawing.Size(631, 529);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Работа с данными";
             // 
-            // label1
+            // addButton
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Введите название типографии";
+            this.addButton.Location = new System.Drawing.Point(366, 338);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(241, 57);
+            this.addButton.TabIndex = 21;
+            this.addButton.Text = "Добавить типографию";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // houseTextBox
+            // 
+            this.houseTextBox.Location = new System.Drawing.Point(394, 259);
+            this.houseTextBox.MaxLength = 7;
+            this.houseTextBox.Name = "houseTextBox";
+            this.houseTextBox.Size = new System.Drawing.Size(213, 27);
+            this.houseTextBox.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(345, 259);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(42, 20);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Дом:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(410, 220);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(155, 20);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "Введите номер дома";
+            // 
+            // streetTextBox
+            // 
+            this.streetTextBox.Location = new System.Drawing.Point(394, 167);
+            this.streetTextBox.MaxLength = 30;
+            this.streetTextBox.Name = "streetTextBox";
+            this.streetTextBox.Size = new System.Drawing.Size(213, 27);
+            this.streetTextBox.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(333, 167);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 20);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Улица:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(394, 133);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(183, 20);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Введите название улицы";
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(394, 74);
+            this.cityTextBox.MaxLength = 30;
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(213, 27);
+            this.cityTextBox.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(333, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Город:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(389, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(188, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Введите название города";
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(84, 356);
+            this.stateTextBox.MaxLength = 30;
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(208, 27);
+            this.stateTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Субъект:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(200, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Введите название субъекта";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(84, 259);
+            this.emailTextBox.MaxLength = 64;
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(208, 27);
+            this.emailTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Почта:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(203, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Введите электронную почту";
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(84, 167);
+            this.phoneNumberTextBox.Mask = "+7-###-###-##-##";
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(208, 27);
+            this.phoneNumberTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Номер:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 76);
+            this.label2.Location = new System.Drawing.Point(19, 133);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Название:";
+            this.label2.Size = new System.Drawing.Size(273, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите номер телефона типографии";
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(93, 73);
-            this.nameTextBox.MaxLength = 60;
+            this.nameTextBox.Location = new System.Drawing.Point(105, 74);
+            this.nameTextBox.MaxLength = 30;
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(248, 27);
+            this.nameTextBox.Size = new System.Drawing.Size(187, 27);
             this.nameTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название:";
+            // 
+            // inputNameLabel
+            // 
+            this.inputNameLabel.AutoSize = true;
+            this.inputNameLabel.Location = new System.Drawing.Point(50, 35);
+            this.inputNameLabel.Name = "inputNameLabel";
+            this.inputNameLabel.Size = new System.Drawing.Size(223, 20);
+            this.inputNameLabel.TabIndex = 0;
+            this.inputNameLabel.Text = "Введите название типографии";
             // 
             // PrintingHouseMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1274, 450);
+            this.ClientSize = new System.Drawing.Size(1425, 596);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.printingHouseDataGridView);
             this.Controls.Add(this.menuStrip1);
@@ -137,6 +341,7 @@ namespace PublishingHouse
             this.Name = "PrintingHouseMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Типографии";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PrintingHouseMenu_FormClosing);
             this.Load += new System.EventHandler(this.PrintingHouseMenu_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -153,10 +358,29 @@ namespace PublishingHouse
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem backTab;
         private System.Windows.Forms.DataGridView printingHouseDataGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Select;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label inputNameLabel;
+        private System.Windows.Forms.MaskedTextBox phoneNumberTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox houseTextBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox streetTextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addButton;
     }
 }
--- PublishingHouse/PublishingHouse/PrintingHouseMenu.cs
+++ PublishingHouse/PublishingHouse/PrintingHouseMenu.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PublishingHouse
 {
@@ -23,10 +26,77 @@ namespace PublishingHouse
 
         private void backTab_Click(object sender, EventArgs e)
         {
+            // Переход к основному меню
             MainMenu mainMenu = new MainMenu();
             Transition.TransitionByForms(this, mainMenu);
         }
 
+        private void PrintingHouseMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
 
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            CorrectInputData();
+        }
+
+        /// <summary>
+        /// Метод проверки правильности введенных данных
+        /// </summary>
+        private void CorrectInputData() 
+        {
+            //if (nameTextBox.Text == "" || !phoneNumberTextBox.MaskCompleted || !IsCorrectEmail(emailTextBox.Text) || !IsCorrectPartOfAddress(stateTextBox.Text)
+            //    || !IsCorrectPartOfAddress(cityTextBox.Text) || !IsCorrectPartOfAddress(stateTextBox.Text) || !IsCorrectNumberOfHouse(houseTextBox.Text))
+
+            
+
+            
+        }
+
+        /// <summary>
+        /// Метод проверки электронной почты на корректность
+        /// </summary>
+        /// <param name="email">Электронная почта</param>
+        /// <returns>Корректна ли электронная почта</returns>
+        private bool IsCorrectEmail(string email)
+        {
+            try
+            {
+                MailAddress mailAddress = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        
+        /// <summary>
+        /// Метод проверки части адреса на корректность
+        /// </summary>
+        /// <param name="part">Часть адреса</param>
+        /// <returns>Корректна ли часть адреса</returns>
+        private bool IsCorrectPartOfAddress(string part) 
+        {
+            if (part.All(Char.IsLetter) && part != "" && Regex.IsMatch(part, "^[А-Яа-я]+$"))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Метод проверки номера дома на корректность
+        /// </summary>
+        /// <param name="house">Номер дома</param>
+        /// <returns>Корректен ли номер дома</returns>
+        private bool IsCorrectNumberOfHouse(string house) 
+        {
+            if (Regex.IsMatch(house, @"^[1-9]\d*(?: ?(?:[А-Га-г]|[/] ?\d+))?$"))
+                return true;
+            else
+                return false;
+        }
     }
 }
