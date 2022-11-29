71b0a4562995cc611e066fc89d1ddfbaec8c34e6
DasDanya <klimachkov.danilka@mail.ru>
2022-11-29 19:41:35 +0300

    Сделал авторизацию пользователей

---
+++ PublishingHouse/PublishingHouse/AdminMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PublishingHouse
+{
+    public partial class AdminMenu : Form
+    {
+        public AdminMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
